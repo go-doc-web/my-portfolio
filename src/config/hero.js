fa15fdab40126da9ffa1e3ca1fe5e76d4a290a51
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-05-30 14:52:14 +0300

    create hero section

---
+++ src/config/hero.js
@@ -0,0 +1,11 @@
+export const heroData = {
+  title: "Oleh Hubskiy",
+  subTitle: "I am a Full Stack Developer",
+  heroDescription: ` I'm a full-stack developer from Ukraine, with a burning passion and a
+        great desire to improve my programming skills.
+        `,
+};
+
+/* My dedication to work and
+        eagerness to learn constantly make me ready for new projects and
+        challenges. */
---
+++ src/db/socials-db.js
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const socialsDB = [
+  {
+    id: uuidv4(),
+    path: "https://www.facebook.com/",
+    name: "facebook",
+    icon: "facebook",
+  },
+  { id: uuidv4(), path: "https://github.com/", name: "github", icon: "github" },
+  {
+    id: uuidv4(),
+    path: "https://www.instagram.com/",
+    name: "instagram",
+    icon: "insta",
+  },
+];
